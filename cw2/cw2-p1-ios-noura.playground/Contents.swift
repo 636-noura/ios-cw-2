3561120a541d47a5e6a414265799386bd5855913
636-noura <66725060+636-noura@users.noreply.github.com>
2020-06-17 23:06:49 +0300

    hi

---
+++ cw2/cw2-p1-ios-noura.playground/Contents.swift
@@ -0,0 +1,18 @@
+
+
+let family = ["wadha" , "sarah" , "hamoud" , "noura" , "salman" ]
+
+var evens = [2, 4, 6, 8, 10, 12 ]
+
+var grades = [ 84.9 , 89.7 , 90.3]
+
+var trueFalse = [ true , true , true , false ]
+
+print(family[0] , family[4])
+
+print(evens[0] , evens[4])
+
+print(grades[0] , grades[2])
+
+print(trueFalse[0] , trueFalse[3])
+
---
+++ cw2/cw2-p3-ios-noura.playground/Contents.swift
@@ -0,0 +1,40 @@
+
+
+var grades = [ 84.9 , 89.7 , 90.3 ]
+
+let average1 = (grades[0] + grades[1] + grades[2] )/3
+
+if average1 >= 90{
+    print("ممتاز")
+}
+    
+else if average1 >= 80{
+    print("جيد جداً")}
+
+else if average1 >= 70{
+        print("جيد")
+}
+
+else{
+    print("راسب")
+}
+
+let average2 = (grades[0] + grades[1] + grades[2] )/3
+
+if average2 >= 90{
+    print("ممتاز")
+}
+    
+else if average2 >= 80{
+    print("جيد جداً")}
+
+else if average2 >= 70{
+        print("جيد")
+}
+    
+else{
+    print("راسب")
+}
+
+let everege2 = ( grades[0]+grades[1] ) / 2
+
